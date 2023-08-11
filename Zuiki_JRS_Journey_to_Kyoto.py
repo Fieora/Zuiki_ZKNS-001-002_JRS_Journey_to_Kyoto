88163e7b7b420a501f9df7d17ba21b9a2b5f57c2
Makusensu <77006845+Makusensu@users.noreply.github.com>
2023-08-11 23:33:40 +0200

    Detection of controller and use ranges of axis
    Dirty detection of both ZKNS-001 and 002.
    Use range of axis instead of precise value to support more controllers (or imperfections?).

--- Zuiki_JRS_Journey_to_Kyoto.py
+++ Zuiki_JRS_Journey_to_Kyoto.py
@@ -3,6 +3,7 @@ import pygame  # install via pip install pygame
 import threading
 import queue
 import os
+import numpy
 
 pyautogui.PAUSE = 0.0571  # Lowest possible delay before the game starts dropping key presses.
 pygame.init()
@@ -120,11 +121,16 @@ def button_pause():
 for i in range(pygame.joystick.get_count()):
     jid = {joyid: i, joyname: pygame.joystick.Joystick(i).get_name()}
     joystick_list.append(jid)
-mascon_select = next((i for i, item in enumerate(joystick_list) if item["joyname"] == "One Handle MasCon for Nintendo Switch Exclusive Edition"),
-                     None)
+mascon_select = next((i for i, item in enumerate(joystick_list) if item["joyname"] == "Nintendo Switch Pro Controller"), None)
 
 if mascon_select is None:
-    print("No Nintendo Switch Pro Controller found. Connect the correct controller and restart the script")
+    for i in range(pygame.joystick.get_count()):
+        jid = {joyid: i, joyname: pygame.joystick.Joystick(i).get_name()}
+        joystick_list.append(jid)
+    mascon_select = next((i for i, item in enumerate(joystick_list) if item["joyname"] == "One Handle MasCon for Nintendo Switch Exclusive Edition"), None)
+
+if mascon_select is None:
+    print("No 'Nintendo Switch Pro Controller' or 'One Handle MasCon for Nintendo Switch Exclusive Edition' found. Connect the correct controller and restart the script")
     exit()
 
 mascon_counter = 99
@@ -167,26 +173,26 @@ try:
                 # Lever section
                 mascon_axis = (joysticks[mascon_select].get_axis(1))
                 mascon_axis = (round(mascon_axis, 2))
-                #print(mascon_axis)
-                if mascon_axis == 0.8:
+                print(mascon_axis)
+                if mascon_axis in numpy.arange(0.8, 0.9):
                     #print("P4")
                     qMascon.put(power_max)
                     mascon_counter = 1
-                if mascon_axis == 0.61:
+                if mascon_axis in numpy.arange(0.6, 0.7):
                     if mascon_counter == 3:
                         qMascon.put(power_inc)
                         mascon_counter = 2
                     if mascon_counter == 1:
                         qMascon.put(power_dec)
                         mascon_counter = 2
-                if mascon_axis == 0.43:
+                if mascon_axis in numpy.arange(0.4, 0.5):
                     if mascon_counter == 4:
                         qMascon.put(power_inc)
                         mascon_counter = 3
                     if mascon_counter == 2:
                         qMascon.put(power_dec)
                         mascon_counter = 3
-                if mascon_axis == 0.24:
+                if mascon_axis in numpy.arange(0.2, 0.3):
                     if mascon_counter == 5:
                         qMascon.put(power_inc)
                         mascon_counter = 4
@@ -196,7 +202,7 @@ try:
                 if mascon_axis == 0.0:
                     qMascon.put(neutral)
                     mascon_counter = 5
-                if mascon_axis == -0.21:
+                if mascon_axis in numpy.arange(-0.2, -0.28):
                     if mascon_counter == 5:
                         qMascon.put(brake_inc)
                         mascon_counter = 6
@@ -205,7 +211,7 @@ try:
                         if notchfix:
                             qMascon.put(brake_dec)
                         mascon_counter = 6
-                if mascon_axis == -0.32:
+                if mascon_axis in numpy.arange(-0.3, -0.38):
                     if mascon_counter == 6:
                         qMascon.put(brake_inc)
                         if notchfix:
@@ -214,7 +220,7 @@ try:
                     if mascon_counter == 8:
                         qMascon.put(brake_dec)
                         mascon_counter = 7
-                if mascon_axis == -0.43:
+                if mascon_axis in numpy.arange(-0.4, -0.48):
                     if mascon_counter == 7:
                         qMascon.put(brake_inc)
                         mascon_counter = 8
@@ -223,7 +229,7 @@ try:
                         if notchfix:
                             qMascon.put(brake_dec)
                         mascon_counter = 8
-                if mascon_axis == -0.53:
+                if mascon_axis in numpy.arange(-0.5, -0.58):
                     if mascon_counter == 8:
                         qMascon.put(brake_inc)
                         if notchfix:
@@ -232,14 +238,14 @@ try:
                     if mascon_counter == 10:
                         qMascon.put(brake_dec)
                         mascon_counter = 9
-                if mascon_axis == -0.64:
+                if mascon_axis in numpy.arange(-0.6, -0.68):
                     if mascon_counter == 9:
                         qMascon.put(brake_inc)
                         mascon_counter = 10
                     if mascon_counter == 11:
                         qMascon.put(brake_dec)
                         mascon_counter = 10
-                if mascon_axis == -0.75:
+                if mascon_axis in numpy.arange(-0.7, -0.78):
                     if mascon_counter == 10:
                         qMascon.put(brake_inc)
                         if notchfix:
@@ -248,7 +254,7 @@ try:
                     if mascon_counter == 12:
                         qMascon.put(brake_dec)
                         mascon_counter = 11
-                if mascon_axis == -0.85:
+                if mascon_axis in numpy.arange(-0.8, -0.9):
                     if mascon_counter == 11:
                         qMascon.put(brake_inc)
                         mascon_counter = 12
